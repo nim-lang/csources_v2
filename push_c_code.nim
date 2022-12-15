c6c0eb027a48b6f8e52dc6801d30a804a7172f8c
ringabout <43030857+ringabout@users.noreply.github.com>
2022-12-15 21:21:46 +0800

    add script

---
+++ push_c_code.nim
@@ -0,0 +1,21 @@
+import std/os
+
+proc exec(cmd: string) =
+  if execShellCmd(cmd) != 0: quit("FAILURE " & cmd)
+
+proc main() =
+  if not dirExists(".git"):
+    quit "[Error] no csources git repository found"
+
+  copyDir("../nimv2/build", getCurrentDir())
+
+  for kind, path in walkDir("c_code"):
+    if kind == pcDir:
+      exec("git add " & path / "*.c")
+
+  exec("git add makefile")
+  exec("git add *.sh")
+  exec("git add *.bat")
+  exec("git add c_code/*.h")
+
+main()
